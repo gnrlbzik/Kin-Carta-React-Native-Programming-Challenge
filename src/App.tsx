2f6db148128033323d21b6e715496c0d5bb3de90
Alesei Narkevitch <github.com@bzik.net>
2021-01-20 03:02:50 -0600

    feat: :sparkles: updating app entry to fetch data and use components to render data

--- src/App.tsx
+++ src/App.tsx
@@ -1,113 +1,74 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {useEffect, useState} from 'react';
 
-const App: () => React$Node = () => {
+import {SafeAreaView, StatusBar, StyleSheet, View} from 'react-native';
+
+import {LOADING_INDICATOR_DURATION} from './constants';
+import {t} from './utils';
+
+import {ContactsList, ContactPreview, Header, LoadingData} from './components';
+
+import {getContacts} from './fetchData';
+
+import type {ContactItem} from './types';
+
+function App() {
+  const [contacts, setContacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [contactDetailsToView, setContactDetailsToView] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      // README: as I chose not to hook up store and actions atm, this will live here for now.
+      // FIXME: should be moved into store/actions/side-effects
+      const getContactsResponse = await getContacts();
+      setContacts(getContactsResponse);
+      setTimeout(() => {
+        setIsLoading(false);
+      }, LOADING_INDICATOR_DURATION);
+    })();
+  }, [contacts.length]);
+
+  const toggleContactDetails = (contactInfo: ContactItem) => {
+    if (contactInfo) {
+      setContactDetailsToView(contactInfo);
+    } else {
+      setContactDetailsToView(null);
+    }
+  };
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
+        <View style={styles.container}>
+          {isLoading ? (
+            <LoadingData />
+          ) : (
+            <>
+              <Header text={t('contactsListHeaderText')} />
+              <ContactsList
+                contacts={contacts}
+                toggleContactDetails={toggleContactDetails}
+              />
+            </>
           )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
+          {contactDetailsToView ? (
+            <ContactPreview
+              contactDetails={contactDetailsToView}
+              toggleContactDetails={toggleContactDetails}
+            />
+          ) : null}
+        </View>
       </SafeAreaView>
     </>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  container: {
+    position: 'relative',
   },
 });
 
